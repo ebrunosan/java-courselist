6a8c24e600b62af6212235d5522549744094c0cc
Bruno Santos <ebrunosan@gmail.com>
2018-07-10 19:46:35 -0400

    tentative to run JSP and JDBC v2

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -6,9 +6,18 @@ import java.net.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "Users",
+        urlPatterns = "/users"
+)
 
 @SuppressWarnings("serial")
 public class Users extends HttpServlet {
@@ -18,7 +27,8 @@ public class Users extends HttpServlet {
 		super();
 	}
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		Connection conn = null;
 		Statement stmt = null;
 
