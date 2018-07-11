180d83b4a4729ceeb56b8f5ddc3d6a54c769ff53
Bruno Santos <ebrunosan@gmail.com>
2018-07-10 21:31:21 -0400

    DbUtil singleton v2

--- src/main/java/DBUtil.java
+++ src/main/java/DBUtil.java
@@ -9,14 +9,14 @@ public class DBUtil {
 	private static Connection connection = null;
 	
 	public static Connection getConnection() throws SQLException {
-        if (connection == null) {
+        //if (connection == null) {
             try {
 				String dbUrl = System.getenv("JDBC_DATABASE_URL");
 				connection = DriverManager.getConnection(dbUrl);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-		}
+		//}
         return connection;
 	}
 }
