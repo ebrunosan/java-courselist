a90f633825c17cc947a1441f6f7f903777345223
Bruno Santos <ebrunosan@gmail.com>
2018-07-15 16:59:41 -0400

    email with helper

--- src/main/java/TestEmailServlet.java
+++ src/main/java/TestEmailServlet.java
@@ -30,16 +30,31 @@ public class TestEmailServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		try {
-		  SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-		  Request request = new Request();
-		  request.setMethod(Method.POST);
-		  request.setEndpoint("mail/send");
-		  request.setBody("{\"personalizations\":[{\"to\":[{\"email\":\"ebrunosan@gmail.com\"}],\"subject\":\"Sending with SendGrid is Fun\"}],\"from\":{\"email\":\"app101305838@heroku.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"and easy to do anywhere, even with Java\"}]}");
-		  Response response = sg.api(request);
-		  System.out.println("+++ EMAIL SENT !!! +++");
-//		  System.out.println(response.getStatusCode());
-//		  System.out.println(response.getBody());
-//		  System.out.println(response.getHeaders());
+			SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+			Request request = new Request();
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			
+			Email from = new Email("app101305838@heroku.com");
+			String subject = "Hello World from the SendGrid Java Library!";
+			Email to = new Email("ebrunosan@gmail.com");
+			Content content = new Content("text/plain", "Hello, Email!");
+			Mail mail = new Mail(from, subject, to, content);
+			
+			request.setBody(mail.build());
+			//request.setBody("{\"personalizations\":[{\"to\":[{\"email\":\"ebrunosan@gmail.com\"}],\"subject\":\"Sending with SendGrid is Fun\"}],\"from\":{\"email\":\"app101305838@heroku.com\"},\"content\":[{\"type\":\"text/plain\",\"value\": \"and easy to do anywhere, even with Java\"}]}");
+			
+			Response response = sg.api(request);
+			
+			System.out.println("+++ EMAIL SENT !!! +++");
+			
+			PrintWriter out = res.getWriter();
+			out.print("<html>");
+			out.write("<p> Email sent sucessfully!!!</p>");
+			out.write("<p>" + response.getStatusCode() + "</p>");
+			//out.write("<p>" + response.statusCode + "</p>");
+			//out.write("<p>" + response.body + "</p>");
+			//out.write("<p>" + response.headers + "</p>");
 		} catch (IOException ex) {
 		  throw ex;
 		}
